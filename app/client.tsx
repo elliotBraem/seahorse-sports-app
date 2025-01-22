1e65e4260321cd8b591446a37f0dd87302fa58b2
Elliot Braem <elliot@ejlbraem.com>
2025-01-21 20:53:55 -0700

    tanstack start wip

---
+++ app/client.tsx
@@ -0,0 +1,9 @@
+/// <reference types="vinxi/types/client" />
+import { StartClient } from "@tanstack/start";
+import { hydrateRoot } from "react-dom/client";
+import "./index.css";
+import { createRouter } from "./router";
+
+const router = createRouter();
+
+hydrateRoot(document!, <StartClient router={router} />);
---
+++ app/router.tsx
@@ -0,0 +1,28 @@
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { routerWithQueryClient } from "@tanstack/react-router-with-query";
+import { routeTree } from "./routeTree.gen";
+
+export function createRouter() {
+  const queryClient = new QueryClient();
+  return routerWithQueryClient(
+    createTanStackRouter({
+      routeTree,
+      defaultPreload: "intent",
+      // Since we're using React Query, we don't want loader calls to ever be stale
+      // This will ensure that the loader is always called when the route is preloaded or visited
+      defaultPreloadStaleTime: 0,
+      context: {
+        auth: undefined,
+        queryClient,
+      },
+    }),
+    queryClient
+  );
+}
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
+}
