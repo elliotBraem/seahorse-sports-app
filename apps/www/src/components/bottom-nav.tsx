40b55183ca419001e5e71ba336e6dc5fd02bdeb3
Elliot Braem <elliot@ejlbraem.com>
2025-01-24 10:54:35 -0700

    footer fix

--- apps/www/src/components/bottom-nav.tsx
+++ apps/www/src/components/bottom-nav.tsx
@@ -38,7 +38,7 @@ export function BottomNav() {
 
   return (
     <nav className="fixed bottom-0 left-0 right-0 z-50 border-gray-800 bg-background shadow-[0_-4px_0_rgba(0,0,0,1)]">
-      <div className="container mx-auto flex h-24 items-center justify-around px-4 md:h-24 max-w-lg">
+      <div className="container mx-auto flex h-20 justify-center items-center gap-4 max-w-sm">
         {links.map((link) => {
           const isActive = currentPath === link.to;
           return (
@@ -46,13 +46,13 @@ export function BottomNav() {
               key={link.to}
               href={link.to}
               className={cn(
-                "flex flex-col items-center justify-center h-16 w-16 rounded-xl bg-white/10 border backdrop-blur-lg shadow-md ",
+                "flex flex-col items-center justify-center h-14 w-14 rounded-xl bg-white/10 border backdrop-blur-lg shadow-md ",
                 isActive
                   ? " text-white border-white"
                   : "border-white/20 hover:border-white text-white/80 hover:text-white",
               )}
             >
-              <FontAwesomeIcon icon={link.icon} size="lg" className="h-7 w-7" />
+              <FontAwesomeIcon icon={link.icon} size="lg" className="h-6 w-6" />
             </Link>
           );
         })}
