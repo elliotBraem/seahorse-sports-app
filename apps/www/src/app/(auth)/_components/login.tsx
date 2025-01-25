6c9b04d638a4ada78d3602d5f9ffaef540a05c26
mzayem <mzayemazam@gmail.com>
2025-01-25 16:15:16 +0500

    hide sign with google button until it's fixed

--- apps/www/src/app/(auth)/_components/login.tsx
+++ apps/www/src/app/(auth)/_components/login.tsx
@@ -11,7 +11,7 @@ export function Login() {
 
   return (
     <div className="flex flex-col gap-3 w-full max-w-sm">
-      <Button
+      {/* <Button
         onClick={loginWithGoogle}
         className="rounded-full"
         size="lg"
@@ -23,7 +23,7 @@ export function Login() {
           </div>
           <span className="flex-1 text-left">Continue with Google</span>
         </div>
-      </Button>
+      </Button> */}
 
       {/* <Button
         onClick={() => {}}
@@ -55,14 +55,16 @@ export function Login() {
         </div>
       </Button> */}
 
-      <div className="relative my-2">
+      {/* <div className="relative my-2">
         <div className="absolute inset-0 flex items-center">
           <span className="w-full border-t" />
         </div>
         <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">or</span>
+          <span className="bg-background p-2 rounded-full text-muted-foreground">
+            or
+          </span>
         </div>
-      </div>
+      </div> */}
 
       <Button
         onClick={() => router.push("/login?type=email")}
